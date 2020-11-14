d084c348c6f94fdf0d0644e27eb11403dce7e669
Kulyk Oleksandr <a_kylik@ukr.net>
2020-11-14 13:12:41 +0200

    add js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+let firstNumber;
+let secondNumber;
+
+do {
+   firstNumber = +prompt(`Введіть перше ціле число`);
+} while (Number.isInteger(firstNumber) === false);
+
+do {
+   secondNumber = +prompt(`Введіть друге ціле число`);
+} while (firstNumber >= secondNumber || Number.isInteger(secondNumber) === false);
+
+const skipEvenNumber = confirm(`Пропускати парні числа?`);
+let sumNumber = 0;
+if (skipEvenNumber) {
+   for (let i = firstNumber; i <= secondNumber; i++) {
+      if (i % 2 === 0) {
+         sumNumber += 0;
+      } else {
+         sumNumber += i;
+      }
+   }
+}
+else {
+   for (let i = firstNumber; i <= secondNumber; i++) {
+      sumNumber += i;
+   }
+}
+
+console.log(`Перше число ${firstNumber}`);
+console.log(`Друге число ${secondNumber}`);
+console.log(`Пропускати парні числа? ${skipEvenNumber}`);
+console.log(`Сума чисел ${sumNumber}`);
